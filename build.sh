61ee48469357391a104842c628b5bc16b329b1c0
Sergey Ivanov <ivanovs-4@yandex.ru>
2019-07-17 19:03:55 +0400

    Build with nix

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 mkdir -p _shake
 set -ex
-nix-shell -shell -p "haskellPackages.ghcWithPackages (pkgs: with pkgs; [shake])"
-  'ghc --make Build.hs -rtsopts -threaded -with-rtsopts=-I0 -outputdir=_shake -o _shake/build'
-_shake/build "$@"
+
+# Build `_shake/build` binary from Build.hs
+# nix-shell -p "haskellPackages.ghcWithPackages (pkgs: with pkgs; [shake])" \
+nix-shell ./shell-shake.nix \
+  --run 'ghc --make Build.hs -rtsopts -threaded -with-rtsopts=-I0 -outputdir=_shake -o _shake/build'
+
+# Build project with `_shake/build`
+nix-shell ./shell.nix --run '_shake/build '"$@"''
