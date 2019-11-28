cf9ddb732d685989954086e0aabdc98e2920ab8b
Sergey Ivanov <ivanovs-4@yandex.ru>
2019-11-28 15:50:05 +0400

    Add burn method

--- Build.hs
+++ Build.hs
@@ -5,8 +5,7 @@ import Development.Shake.Util
 
 main :: IO ()
 main = shakeArgs shakeOptions{shakeFiles="_build"} $ do
-    -- want ["firmware"]
-    want ["_build/csource/firmware"]
+    want [firmware_bin]
 
     phony "clean" $ do
         putNormal "Cleaning files in _build"
@@ -25,8 +24,14 @@ main = shakeArgs shakeOptions{shakeFiles="_build"} $ do
         cmd (Cwd "ionic") "nix-shell ../ionic-shell.nix --run"
             ["cabal new-run -- ionic ../_build/csource"]
 
-    -- "firmware" %> \out -> do
-    "_build/csource/firmware" %> \out -> do
+    -- For this to work you have to create `udev` rules like in ./nix/stlink.nix
+    -- For nixos users: add import of ./nix/stlink.nix to /etc/nixos/configuration.nix
+    -- and add user to `plugdev` group
+    phony "burn" $ do
+        need [firmware_bin]
+        cmd (Cwd "_build/csource") "nix-shell ../../stlink-shell.nix --run" ["make burn"]
+
+    firmware_bin %> \out -> do
         need ["csource"]
         copyFileChanged "csource/Makefile.src" "_build/csource/Makefile"
         cmd (Cwd "_build/csource") "nix-shell ../../gcc-shell.nix --run" ["make"]
@@ -43,3 +48,6 @@ main = shakeArgs shakeOptions{shakeFiles="_build"} $ do
 --         () <- cmd "sh ./run-in-shell.sh ./gcc-shell.nix arm-none-eabi-gcc -c"
 --               [c] "-o" [out] "-MMD -MF" [m]
 --         needMakefileDependencies m
+
+  where
+    firmware_bin = "_build/csource/firmware.bin"
